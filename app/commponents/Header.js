fa92b95799dc58f9eed35e377a355117e7b1f45b
unknown <rizwanahmedg2020@gmail.com>
2023-09-22 10:06:28 -0700

    remove extre commtint

--- app/commponents/Header.js
+++ app/commponents/Header.js
@@ -11,12 +11,12 @@ export default function Header() {
                         </svg>
                         <span className="ml-3 text-xl">Stock Management</span>
                     </a>
-                    <nav className="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
+                    {/* <nav className="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
                         <a className="mr-5 hover:text-gray-900">First Link</a>
                         <a className="mr-5 hover:text-gray-900">Second Link</a>
                         <a className="mr-5 hover:text-gray-900">Third Link</a>
                         <a className="mr-5 hover:text-gray-900">Fourth Link</a>
-                    </nav>
+                    </nav> */}
                     
                 </div>
             </header>
